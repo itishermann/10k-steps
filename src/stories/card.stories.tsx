a745c55f50bbf633ea5d403a0e1dd61815cef70c
Hermann Kao <38349706+itishermann@users.noreply.github.com>
2025-01-17 18:13:51 +0100

    feat(docs): add stories to ui components

---
+++ src/stories/card.stories.tsx
@@ -0,0 +1,37 @@
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof Card> = {
+	title: "ui/Card",
+	component: Card,
+	tags: ["autodocs"],
+	argTypes: {},
+};
+export default meta;
+
+type Story = StoryObj<typeof Card>;
+
+export const Base: Story = {
+	render: (args) => (
+		<Card {...args}>
+			<CardHeader>
+				<CardTitle>Card Title</CardTitle>
+				<CardDescription>This is the card description.</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<p>This is the card content.</p>
+			</CardContent>
+			<CardFooter>
+				<p>Card Footer</p>
+			</CardFooter>
+		</Card>
+	),
+	args: {},
+};
---
+++ src/stories/spinner.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Spinner } from "@/components/ui/spinner";
+
+const meta: Meta<typeof Spinner> = {
+	title: "ui/Spinner",
+	component: Spinner,
+	tags: ["autodocs"],
+	argTypes: {},
+};
+export default meta;
+
+type Story = StoryObj<typeof Spinner>;
+
+export const Small: Story = {
+	render: (args) => <Spinner {...args}>Label</Spinner>,
+	args: {
+		size: "small",
+	},
+};
+export const Medium: Story = {
+	render: (args) => <Spinner {...args}>Label</Spinner>,
+	args: {
+		size: "medium",
+	},
+};
+export const Large: Story = {
+	render: (args) => <Spinner {...args}>Label</Spinner>,
+	args: {
+		size: "large",
+	},
+};
