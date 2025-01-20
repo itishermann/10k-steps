f3ab4f88618b3d61bafd2da37eb3538f40b4fb7e
Hermann Kao <38349706+itishermann@users.noreply.github.com>
2025-01-20 13:03:50 +0100

    feat(theme): fix dark theme hydratation warning when server rendering

--- src/stories/button.stories.tsx
+++ src/stories/button.stories.tsx
@@ -1,59 +1,103 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { Loader2, Mail } from "lucide-react";
-
 import { Button } from "@/components/ui/button";
+import type { Meta, StoryObj } from "@storybook/react";
+import { Mail } from "lucide-react";
 
 const meta: Meta<typeof Button> = {
 	title: "ui/Button",
 	component: Button,
 	tags: ["autodocs"],
-	argTypes: {},
+	argTypes: {
+		variant: {
+			control: { type: "select" },
+			options: [
+				"default",
+				"destructive",
+				"outline",
+				"secondary",
+				"ghost",
+				"link",
+			],
+		},
+		size: {
+			control: { type: "select" },
+			options: ["default", "sm", "lg", "icon"],
+		},
+		effect: {
+			control: { type: "select" },
+			options: [
+				"expandIcon",
+				"ringHover",
+				"shine",
+				"shineHover",
+				"gooeyRight",
+				"gooeyLeft",
+				"underline",
+				"hoverUnderline",
+			],
+		},
+		loading: { control: "boolean" },
+		disabled: { control: "boolean" },
+		iconPlacement: {
+			control: { type: "select" },
+			options: ["left", "right"],
+		},
+	},
 };
 export default meta;
 
 type Story = StoryObj<typeof Button>;
 
-export const Base: Story = {
-	render: (args) => <Button {...args}>Button</Button>,
-	args: {},
-};
-export const Outline: Story = {
+export const Default: Story = {
 	render: (args) => <Button {...args}>Button</Button>,
 	args: {
-		variant: "outline",
+		variant: "default",
+		size: "default",
+		loading: false,
+		disabled: false,
 	},
 };
-export const Ghost: Story = {
-	render: (args) => <Button {...args}>Button</Button>,
-	args: {
-		variant: "ghost",
-	},
-};
-export const Secondary: Story = {
-	render: (args) => <Button {...args}>Button</Button>,
+
+export const WithIcon: Story = {
+	render: (args) => <Button {...args}>Login with Email Button</Button>,
 	args: {
 		variant: "secondary",
+		size: "default",
+		icon: Mail,
+		iconPlacement: "left",
 	},
 };
-export const Link: Story = {
-	render: (args) => <Button {...args}>Button</Button>,
-	args: {
-		variant: "link",
-	},
-};
+
 export const Loading: Story = {
 	render: (args) => <Button {...args}>Button</Button>,
 	args: {
 		loading: true,
 	},
 };
-export const WithIcon: Story = {
-	render: (args) => (
-		<Button {...args}>
-			<Mail className="mr-2 h-4 w-4" /> Login with Email Button
-		</Button>
-	),
+
+export const Outline: Story = {
+	render: (args) => <Button {...args}>Button</Button>,
+	args: {
+		variant: "outline",
+	},
+};
+
+export const Ghost: Story = {
+	render: (args) => <Button {...args}>Button</Button>,
+	args: {
+		variant: "ghost",
+	},
+};
+
+export const Secondary: Story = {
+	render: (args) => <Button {...args}>Button</Button>,
 	args: {
 		variant: "secondary",
+	},
+};
+
+export const Link: Story = {
+	render: (args) => <Button {...args}>Button</Button>,
+	args: {
+		variant: "link",
 	},
 };
