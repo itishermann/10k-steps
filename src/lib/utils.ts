c77043f46a939ed1ec59dbd62416af791674dc78
Hermann Kao <38349706+itishermann@users.noreply.github.com>
2025-01-25 09:49:27 +0100

    feat(step-length-estimation): add utility functions to estimate step length

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,25 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+/**
+ * Calculate step length based on height
+ * @param height - height in cm
+ * @param gender
+ */
+export function calculateStepLength(height: number, gender: "male" | "female") {
+	const factor = gender === "male" ? 0.415 : 0.413;
+	return height * factor;
+}
+
+/**
+ * Calculate distance based on step length and step amount and convert to kilometers
+ * @param stepLength - step length in cm
+ * @param stepAmount - step amount
+ */
+export function calculateDistance(stepLength: number, stepAmount: number) {
+	return (stepLength * stepAmount) / 10000; // Convert to kilometers
 }
