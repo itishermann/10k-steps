6217b76be2bba8d88bbdf06d1fd3f0fe463dc1bb
Hermann Kao <38349706+itishermann@users.noreply.github.com>
2025-02-12 15:19:40 +0100

    feat(instruction-item): enhance duration and distance display with improved formatting

--- src/components/molecules/navigator/instruction-item.tsx
+++ src/components/molecules/navigator/instruction-item.tsx
@@ -10,34 +10,45 @@ interface InstructionItemProps {
 export function InstructionItem({
 	step: { instruction, duration, distance, type, name },
 }: InstructionItemProps) {
+	const durationInMinutes = Math.floor(duration / 60);
+	const durationInSeconds = Math.floor(duration);
+	const showDuration = durationInMinutes > 0 || durationInSeconds > 0;
+	const durationUnit = durationInMinutes > 0 ? "minute" : "second";
+	const durationValue =
+		durationInMinutes > 0 ? durationInMinutes : durationInSeconds;
+	const isDurationPlural = durationValue > 1;
+	const durationText = `${durationValue} ${durationUnit}${isDurationPlural ? "s" : ""}`;
+	const distanceInKm = Math.floor(distance / 1000);
+	const distanceInM = Math.floor(distance);
+	const distanceUnit = distanceInKm > 0.5 ? "km" : "m";
+	const distanceValue = distanceInKm > 0.5 ? distanceInKm : distanceInM;
+	const distanceText = `${distanceValue} ${distanceUnit}`;
 	return (
 		<Card className="h-fit">
 			<CardContent className="grid grid-cols-3 grid-rows-3 p-2 gap-1">
 				<div className="row-span-2 flex items-center justify-center">
-					<span
-						className="text-2xl line-clamp-1"
+					<b
+						className="text-2xl line-clamp-1 font-bold"
 						// biome-ignore lint/security/noDangerouslySetInnerHtml: the html is sanitized
 						dangerouslySetInnerHTML={createMarkup(
 							instructionCodeToSymbol[type],
 						)}
 					/>
 				</div>
-				<div className="col-start-1 row-start-3 flex items-center justify-center ">
-					{distance > 0.5 ? `${Math.floor(distance)}m` : ""}
-				</div>
+				<b className="col-start-1 row-start-3 flex items-center justify-center text-lg">
+					{distanceText}
+				</b>
 				<div className="col-span-2 col-start-2 row-start-1">
-					{name && name !== "-" ? `on ${name}` : ""}
+					{name && name !== "-" ? "on" : ""}
+					{name && name !== "-" && <b> {name}</b>}
 				</div>
 				<div className="col-span-2 col-start-2 row-start-2">
 					{/* biome-ignore lint/security/noDangerouslySetInnerHtml: the html is sanitized */}
 					<span dangerouslySetInnerHTML={createMarkup(instruction)} />
 				</div>
-				<div className="col-span-2 row-start-3">
-					{/* duration formatted in minutes or seconds if less than a minute */}
-					{duration < 60
-						? `${Math.floor(duration)} seconds`
-						: `${Math.floor(duration / 60)} minutes`}
-				</div>
+				<small className="col-span-2 row-start-3">
+					{showDuration ? `${durationText}` : ""}
+				</small>
 			</CardContent>
 		</Card>
 	);
